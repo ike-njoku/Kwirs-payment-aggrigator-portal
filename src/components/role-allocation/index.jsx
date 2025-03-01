9422e2b624825ec9f09b861b1e411fe25752d2d9
daluclemas <chidaluanukam@gmail.com>
2025-03-01 15:05:06 +0100

    update: remove create button

--- src/components/role-allocation/index.jsx
+++ src/components/role-allocation/index.jsx
@@ -92,18 +92,7 @@ const ResourcesPage = () => {
         <div className=" w-[90%] mx-auto py-5">
           <div className=" w-full lg:mt-10">
             {/* search bar and filter options here */}
-            <section className="w-full mb-3 flex justify-end items-center gap-5 lg:justify-start">
-              <button
-                id="dropdownBgHoverButton"
-                data-dropdown-toggle="dropdownBgHover"
-                className="text-pumpkin focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  relative  gap-2 border border-pumpkin"
-                type="button"
-                onClick={handleOpenCreateResourceModal}
-              >
-                Create Resource
-                <FaPlus />
-              </button>
-            </section>
+
             {/* table */}
             <UsersTable isRoleAllocation={true} />
           </div>
