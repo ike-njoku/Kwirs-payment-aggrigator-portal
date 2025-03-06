ea9bcdcb1a790724f9d70cee7333d5f60d2894b0
ken <icodejsx>
2025-03-06 17:39:54 +0100

    doing somehing already and its working

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import Head from "next/head";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +30,12 @@ export default function RootLayout({ children }) {
           sizes="16x16"
         />
       </Head>
+      <Script src="https://pay-service.icadpay.com/host/new-inline-stage-pay.js" strategy="lazyOnload" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ToastContainer />
+        <Script src="https://pay-service.icadpay.com/host/new-inline-stage-pay.js" strategy="lazyOnload" />
         {children}
       </body>
     </html>
--- src/components/home/SelectPaymentGateway.jsx
+++ src/components/home/SelectPaymentGateway.jsx
@@ -1,49 +1,44 @@
 "use client";
 import React, { useState } from "react";
 import GatewayRadio from "../shared-components/inputs/GatewayRadio";
-import visa from "../../../public/images/icad-logo.png";
+import PaymentButtons from "../shared-components/buttons/PaymentButtons";
+
+import icadpay from "../../../public/images/icad-logo.png";
 import remita from "../../../public/images/remita.png";
 import interswitch from "../../../public/images/interswitch.png";
 import etranzact from "../../../public/images/etranzact.jpg";
 import flutterWave from "../../../public/images/Flutterwave-Logo.jpg";
-import PaymentButtons from "../shared-components/buttons/PaymentButtons";
+import IcadPayModal from "@/services/IcadPayModal";
 
 const SelectPaymentGateway = ({ showPreviousComponent }) => {
   const [selectedOption, setSelectedOption] = useState("nil");
+  const [showModal, setShowModal] = useState(false);
 
   const handleSelectGateway = (e) => {
-    const { name, value } = e.target;
+    const value = e.target.value;
     setSelectedOption(value);
+
+    if (value === "icadpay") {
+      setShowModal(true);
+    }
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedOption("nil");
   };
 
   const paymentGateways = [
-    {
-      name: "visa",
-      img: visa,
-    },
-    {
-      name: "remita",
-      img: remita,
-    },
-    {
-      name: "interswitch",
-      img: interswitch,
-    },
-    {
-      name: "etranzact",
-      img: etranzact,
-    },
-    {
-      name: "flutterWave",
-      img: flutterWave,
-    },
+    { name: "icadpay", img: icadpay },
+    { name: "remita", img: remita },
+    { name: "interswitch", img: interswitch },
+    { name: "etranzact", img: etranzact },
+    { name: "flutterWave", img: flutterWave },
   ];
 
   return (
     <section className="w-full md:max-w-[450px] sm:mx-auto md:mx-0 md:ml-auto pt-8 pb-5 px-8 md:px-10 rounded-[28px] border border-pumpkin mt-10">
-      <h3
-        className={`font-bold sm:text-3xl capitalize text-center text-2xl text-white `}
-      >
+      <h3 className="font-bold sm:text-3xl capitalize text-center text-2xl text-white">
         Select Payment Method
       </h3>
 
@@ -57,10 +52,15 @@ const SelectPaymentGateway = ({ showPreviousComponent }) => {
         />
       ))}
 
-      <div className="w-full md:max-w-[450px] sm:mx-auto md:mx-0 md:ml-auto  flex justify-between gap-4 items-center mt-6">
+      <div className="w-full flex justify-between gap-4 items-center mt-6">
         <PaymentButtons label="Back" onClick={showPreviousComponent} />
-        {/* <PaymentButtons onClick={showNextComponent} /> */}
       </div>
+
+      {/* iCadPay Modal */}
+      <IcadPayModal isOpen={showModal} onClose={closeModal} />
+
+      
+      
     </section>
   );
 };
