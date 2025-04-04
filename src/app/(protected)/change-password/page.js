abe71e24cb9ea02b409c8491e73296da2686f717
Precious Ojimadu <precious13th@outlook.com>
2025-04-04 22:54:54 +0100

    Change password creation

---
+++ src/app/(protected)/change-password/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ChangePasswordPage from "../../../components/change-password";
+
+const page = () => {
+  return <ChangePasswordPage />;
+};
+
+export default page;
