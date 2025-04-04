24be88388dae9d19ed924744c6eee0ac1e1dabfe
Precious Ojimadu <precious13th@outlook.com>
2025-04-04 13:17:27 +0100

    Transactions List Creation

---
+++ src/app/(protected)/transaction-list/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TransactionListPage from "../../../components/transaction-list";
+
+const page = () => {
+  return <TransactionListPage />;
+};
+
+export default page;
