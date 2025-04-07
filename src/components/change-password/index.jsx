7daef8e8c9cf1f8fa18c83142fba574c2cdb9683
Precious Ojimadu <precious13th@outlook.com>
2025-04-07 22:34:31 +0100

    reset password and chapcha integration

--- src/components/change-password/index.jsx
+++ src/components/change-password/index.jsx
@@ -1,3 +1,4 @@
+
 "use client";
 import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/navigation";
@@ -137,3 +138,4 @@ const ChangePasswordPage = () => {
 };
 
 export default ChangePasswordPage;
+
