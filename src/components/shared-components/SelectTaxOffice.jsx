b3e5495dbb325a74a109407ad6095598499c7c98
StephenChi-hi <nwikechisom77@gmail.com>
2025-04-15 22:44:42 +0100

    fixed

--- src/components/shared-components/SelectTaxOffice.jsx
+++ src/components/shared-components/SelectTaxOffice.jsx
@@ -46,15 +46,11 @@ const SelectTaxOffice = ({ taxOfficeId, setTaxOfficeId }) => {
         >
           <option value="">Select Tax Office</option>
           <option value="KUB">Test tax Office</option>
-          {taxOffices.length > 0 ? (
-            taxOffices.map((office) => (
-              <option key={office.TaxOfficeId} value={office.TaxOfficeId}>
-                {office.description}
-              </option>
-            ))
-          ) : (
-            <option disabled>Loading...</option>
-          )}
+          {taxOffices.map((office) => (
+            <option key={office.TaxOfficeId} value={office.TaxOfficeId}>
+              {office.TaxOfficeName}
+            </option>
+          ))}
         </select>
       </div>
     </div>
