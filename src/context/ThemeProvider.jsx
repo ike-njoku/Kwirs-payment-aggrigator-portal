456dd81ab226825c52455b3d5ec2ba88bf5c1c01
ikenjokudc <ikenjokudc@gmail.com>
2025-05-21 16:19:37 -0400

    fix: try updates

--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -4,7 +4,10 @@ import React, { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-  const themeInStorage = window.localStorage.getItem("theme") || "light";
+  const themeInStorage =
+    typeof window !== "undefined"
+      ? localStorage.getItem("theme") || "light"
+      : "light"; // Fixed: Added check for window
   const [theme, setTheme] = useState(themeInStorage); // Fixed: Initialize with themeInStorage
   const toggleTheme = (mode) => {
     setTheme(mode);
